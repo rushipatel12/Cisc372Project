5879dca3f08d0f4ad5fe12015bfb25793656778a
Rushi Patel <prushir@udel.edu>
2019-11-24 00:09:32 -0500

    Added screenshots to each folder

--- MonteCarlo/OMP/monte_pi.c
+++ MonteCarlo/OMP/monte_pi.c
@@ -5,40 +5,41 @@
 #include <math.h>
 #include <string.h>
 #include <omp.h>
-//#define SEED[16] 35791246
 
 int main(int argc, char** argv)
 {
     double start = omp_get_wtime();
     int threads = 4;
-   int niter=1000000000;
-   double x,y;
-   int i; /* # of points in the 1st quadrant of unit circle */
-   double z;
+    int niter = 1000000000;
+    double x,y;
+    int i; 
+    double z;
 
 	int seed[threads];
 	for(int j=0; j<threads; j++){
-		seed[j]=rand();
+	    seed[j]=rand();
 	}
-   /* initialize random numbers */
-   //srand(SEED);
+
    omp_set_num_threads(threads);
+
    int s;
    int count = 0;
    #pragma omp parallel private(s,x,y,z)shared(count,niter)
    {
-   s=seed[omp_get_thread_num()];
-   #pragma omp for reduction(+:count)
-   for ( i=0; i<niter; i++) {
-      x = (double)rand_r(&s)/RAND_MAX;
-      y = (double)rand_r(&s)/RAND_MAX;
-      z = x*x+y*y;
-      if (z<=1) count++;
-      }
-	}
-	double pi;
-   pi=(double)count/niter*4;
+       s=seed[omp_get_thread_num()];
+        #pragma omp for reduction(+:count)
+        for ( i=0; i<niter; i++) {
+            x = (double)rand_r(&s)/RAND_MAX;
+            y = (double)rand_r(&s)/RAND_MAX;
+            z = x*x+y*y;
+            if (z<=1) {
+                count++;
+            }
+        }
+    }
+    double pi;
+    pi=(double)count/niter*4;
     double total = omp_get_wtime() - start;
-   printf("# of trials= %d , estimate of pi is %g , time taken = %f \n",niter,pi,total);
-   return 0;
+    printf(" Number of Trials = %d , Estimate of Pi is %f , Time Taken = %f , Number of Threads = %d \n",niter,pi,total,threads);
+    return 0;
 }
--- MonteCarlo/Serial/monte_pi.c
+++ MonteCarlo/Serial/monte_pi.c
@@ -10,24 +10,23 @@
 int main(int argc, char** argv)
 {
    double start = omp_get_wtime();
-   int niter=1000000000;
+   int niter = 1000000000;
    double x,y;
-   int i,count=0; /* # of points in the 1st quadrant of unit circle */
+   int i,count=0; 
    double z;
    double pi;
-
-
-   /* initialize random numbers */
    srand(SEED);
    count=0;
    for ( i=0; i<niter; i++) {
       x = (double)rand()/RAND_MAX;
       y = (double)rand()/RAND_MAX;
       z = x*x+y*y;
-      if (z<=1) count++;
+      if (z<=1){
+         count++;
       }
+   }
    pi=(double)count/niter*4;
    double total = omp_get_wtime() - start;
-   printf("# of trials= %d , estimate of pi is %g , time taken = %f \n",niter,pi,total);
+   printf("Number of Trials = %d , Estimate of Pi is %f , Time Taken = %f \n",niter,pi,total);
    return 0;
 }
